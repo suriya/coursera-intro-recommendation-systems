9b82a53d56a6d08af2533f3b117b15caa8269eb7
Suriya Subramanian <suriya@alumni.cs.utexas.edu>
2013-10-31 21:51:49 +0530

    Implemented TagEntropyMetric.

--- pa4-eval-assignment/src/main/java/edu/umn/cs/recsys/TagEntropyMetric.java
+++ pa4-eval-assignment/src/main/java/edu/umn/cs/recsys/TagEntropyMetric.java
@@ -1,7 +1,9 @@
 package edu.umn.cs.recsys;
 
 import com.google.common.collect.ImmutableList;
+
 import edu.umn.cs.recsys.dao.ItemTagDAO;
+
 import org.grouplens.lenskit.core.LenskitRecommender;
 import org.grouplens.lenskit.eval.algorithm.AlgorithmInstance;
 import org.grouplens.lenskit.eval.data.traintest.TTDataSet;
@@ -11,10 +13,21 @@ import org.grouplens.lenskit.eval.metrics.topn.ItemSelectors;
 import org.grouplens.lenskit.eval.traintest.TestUser;
 import org.grouplens.lenskit.scored.ScoredId;
 import org.grouplens.lenskit.vectors.MutableSparseVector;
+import org.grouplens.lenskit.vectors.SparseVector;
 import org.grouplens.lenskit.vectors.VectorEntry;
+import org.grouplens.lenskit.vectors.VectorEntry.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
+
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * A metric that measures the tag entropy of the recommended items.
@@ -24,9 +37,10 @@ public class TagEntropyMetric extends AbstractTestUserMetric {
     private final int listSize;
     private final List<String> columns;
 
+    private static final Logger logger = LoggerFactory.getLogger(TagEntropyMetric.class);
     /**
      * Construct a new tag entropy metric.
-     * 
+     *
      * @param nitems The number of items to request.
      */
     public TagEntropyMetric(int nitems) {
@@ -72,6 +86,76 @@ public class TagEntropyMetric extends AbstractTestUserMetric {
         private double totalEntropy = 0;
         private int userCount = 0;
 
+        private Set<Long> movieTagIds(TagVocabulary vocab, ItemTagDAO tagDAO, long movie) {
+            Set<Long> tagIds = new HashSet<Long>();
+            List<String> movieTags = tagDAO.getItemTags(movie);
+            for (String tag : movieTags) {
+                long tagid = vocab.getTagId(tag);
+                tagIds.add(tagid);
+            }
+            return tagIds;
+        }
+
+        private Map<Long, Set<Long>> movieTagIdMap(TagVocabulary vocab, ItemTagDAO tagDAO, List<ScoredId> recommendations) {
+            Map<Long, Set<Long>> moviemap = new HashMap<Long, Set<Long>>();
+            for (ScoredId scoredId : recommendations) {
+                long movie = scoredId.getId();
+                Set<Long> tagIds = movieTagIds(vocab, tagDAO, movie);
+                moviemap.put(movie, tagIds);
+            }
+            return moviemap;
+        }
+
+        /**
+         * A vector of tagids just within the movies in the set.
+         * @param recommendations
+         * @return
+         */
+        private MutableSparseVector moviesetVocab(TagVocabulary vocab, ItemTagDAO tagDAO, Map<Long, Set<Long>> moviemap) {
+            Set<Long> subsetVocab = new HashSet<Long>();
+            for (long movie : moviemap.keySet()) {
+                Set<Long> tagids = moviemap.get(movie);
+                for (long tagid : tagids) {
+                    subsetVocab.add(tagid);
+                }
+            }
+            return MutableSparseVector.create(subsetVocab);
+        }
+
+        private MutableSparseVector tagProbabilities(TagVocabulary vocab, ItemTagDAO tagDAO, List<ScoredId> recommendations) {
+            Map<Long, Set<Long>> moviemap = movieTagIdMap(vocab, tagDAO, recommendations);
+            double movieCount = moviemap.size();
+            assert (movieCount == recommendations.size());
+            MutableSparseVector tagProbabilities = moviesetVocab(vocab, tagDAO, moviemap);
+            logger.info("Tag vocabulary size: {}", tagProbabilities.keyDomain().size());
+            for (VectorEntry entry : tagProbabilities.fast(State.EITHER)) {
+                long tagid = entry.getKey();
+                double probability = 0.0;
+                for (long movie : moviemap.keySet()) {
+                    Set<Long> movietags = moviemap.get(movie);
+                    double numTags = movietags.size();
+                    if (movietags.contains(tagid)) {
+                        probability += (1.0 / movieCount) * (1.0 / numTags);
+                    }
+                }
+                tagProbabilities.set(entry, probability);
+            }
+            return tagProbabilities;
+        }
+
+
+        private double entropy(TagVocabulary vocab, ItemTagDAO tagDAO, List<ScoredId> recommendations) {
+            MutableSparseVector tagProbs = tagProbabilities(vocab, tagDAO, recommendations);
+            MutableSparseVector logProbs = MutableSparseVector.create(tagProbs.keyDomain());
+            for (VectorEntry entry : logProbs.fast(State.UNSET)) {
+                long tagid = entry.getKey();
+                double probability = tagProbs.get(tagid);
+                double logprob = Math.log(probability) / Math.log(2);
+                logProbs.set(entry, logprob);
+            }
+            return -1.0 * logProbs.dot(tagProbs);
+        }
+
         /**
          * Evaluate a single test user's recommendations or predictions.
          * @param testUser The user's recommendation result.
@@ -91,9 +175,9 @@ public class TagEntropyMetric extends AbstractTestUserMetric {
             ItemTagDAO tagDAO = lkrec.get(ItemTagDAO.class);
             TagVocabulary vocab = lkrec.get(TagVocabulary.class);
 
-            double entropy = 0;
-
-            // TODO Implement the entropy metric
+            logger.info("Got recommendations: {}", recommendations.size());
+            double entropy = entropy(vocab, tagDAO, recommendations);
+            logger.info("Entropy: {}", entropy);
 
             totalEntropy += entropy;
             userCount += 1;
